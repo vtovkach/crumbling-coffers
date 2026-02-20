d3a8a2eda4daecc47709719d980eb5248c0aa04a
Pope Vaditto IX <vadim003600@gmail.com>
2026-02-19 23:04:06 -0600

    PROJ-24 Created main.c file which will serve as the entry point to the server.
    1.) Redirected stderr file to separate log file. That log file will track all server's errors.
    
    2.) Created a child process with 'fork' syscall. Later, once I set up the orchestrator process, I will use 'execve' syscall to launch the orchestrator process.

---
+++ server/supervisor/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#include "../include/common/util.h"
+
+#define LOG_FILE "log/log.txt"
+
+int main(void)
+{  
+    if(redirect_stderr(LOG_FILE) < 0)
+    {
+        char time[TIME_BUFFER_SIZE];
+        getTime(time, TIME_BUFFER_SIZE);
+        fprintf(stderr, "%s 'redirect_stderr' failed: %s\n", time, strerror(errno));
+        return -1;
+    }
+
+    // Create Orchestrator Process 
+    pid_t orchestrator_process = fork(); 
+    
+    // Check if fork failed 
+    if(orchestrator_process < 0)
+    {
+        char time[TIME_BUFFER_SIZE];
+        getTime(time, TIME_BUFFER_SIZE);
+        fprintf(stderr, "%s 'redirect_stderr' failed: %s\n", time, strerror(errno));
+        return -1;
+    }
+    
+    // Here goes the child process 
+    if(orchestrator_process == 0)
+    {
+        // Spawn orchestrator 
+        // TODO 
+
+        // execve(...)
+
+        _exit(0);
+    }
+
+    waitpid(orchestrator_process, NULL, 0);
+
+    return 0;
+}
