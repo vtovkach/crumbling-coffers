8a770276bf20f9656bf666daad50cbbb74c3182f
Berlin <vadim003600@gmail.com>
2026-02-20 20:18:08 -0600

    PROJ-30 Added initial code to the orchestrator process to see if 'excev' syscall works properly.

--- server/orchestrator/orchestrator.c
+++ server/orchestrator/orchestrator.c
@@ -1,6 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
 
 int main(void)
 {
+    int counter = 0;
+
+    for(;;)
+    {
+        sleep(2);
+        printf("Tick: %d\n", counter++);
+    }
 
     return 0;
 }
