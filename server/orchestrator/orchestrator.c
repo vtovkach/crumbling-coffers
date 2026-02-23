33fdc9de8071e5d955357bd5af03f3a05431724d
V <vadim003600@gmail.com>
2026-02-22 19:00:10 -0600

    PROJ-41 Establish listening TCP socket and bind it to certain port.
    Use getaddrinfo to automatically obtain all necessary data that is used to bind socket to a specific port.
    
    Create socket and bind it to certain port to accept connection from the clients.
    
    I set binded port to be immediately reusable after server is closed to prevent kernels shutdown time down.
    
    Make listening socket to non-blocking, so later I can use epoll to monitor for connections.

--- server/orchestrator/orchestrator.c
+++ server/orchestrator/orchestrator.c
@@ -64,6 +64,43 @@ int main(int argc, char *argv[])
         return 1; 
     }
 
+    // Establish and bind listening socket to designated PORT 
+    
+    int listen_fd; 
+    struct addrinfo *listen_ai = NULL;
+    struct addrinfo hint;  
+
+    memset(&hint, 0, sizeof(hint));
+    hint.ai_family = AF_INET;
+    hint.ai_socktype = SOCK_STREAM;
+    hint.ai_protocol = 0; 
+    hint.ai_flags = AI_PASSIVE;
+
+    if(getaddrinfo(NULL, SERVER_TCP_PORT, &hint, &listen_ai) != 0)
+    {
+        perror("getaddrinfo (orchestrator)");
+        kill(p_pid, SIGUSR2);
+        return 1; 
+    }
+
+    listen_fd = socket(listen_ai->ai_family, listen_ai->ai_socktype, listen_ai->ai_protocol);
+    if(listen_fd < 0)
+        goto error; 
+
+    int opt = 1; 
+    if(setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+        goto error; 
+
+    if(bind(listen_fd, listen_ai->ai_addr, listen_ai->ai_addrlen) == -1)
+        goto error;
+
+    if(listen(listen_fd, MAX_TCP_QUEUE) == -1)
+        goto error; 
+    
+    int fl = fcntl(listen_fd, F_GETFL, 0);
+    if(fl < 0 || fcntl(listen_fd, F_SETFL, fl | O_NONBLOCK))
+        goto error; 
+    
     for(;;)
     {
         // Check if parent wants to terminate the process 
