2bc3089a07dcc8af906378e5c411e7bde46f29c1
V <vadim003600@gmail.com>
2026-02-25 10:50:34 -0600

    PROJ-41 Added buffer content display in addClientToQueue function to see if the buffer is properly set.

--- server/orchestrator/orchestrator.c
+++ server/orchestrator/orchestrator.c
@@ -69,11 +69,22 @@ void set_terminate(int sig)
 }
 
 
-int addClientToQueue()
+int addClientToQueue(struct Client *client)
 {
     // Just a place holder for now
     // TODO 
     
+    printf("Buffer's Content: ");
+    for(int i = 0; i < TCP_SEGMENT_SIZE; i++)
+    {
+        char cur_char = (char) client->buffer[i];
+        if(cur_char == '\0') { cur_char = '*'; } // Indicate pending zero 
+        
+        putchar(cur_char);
+    }
+    putchar('\n');
+    fflush(stdout);
+
     return 0;
 }
 
