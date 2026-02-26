3e05c05c8b2464829f596c0a265c52faf72a9c8d
Daniel Pelley <daniel.pelley@outlook.com>
2026-02-26 13:35:12 -0600

    PROJ-21: Common item added.
    created the basic common item for players to pick up in game.

--- crumbling-coffers/Scripts/common_item.gd
+++ crumbling-coffers/Scripts/common_item.gd
@@ -1,7 +1,9 @@
 extends "res://Scripts/pickup_base.gd"
 
 func _ready() -> void:
+	super()
 	points = 5
 
 func on_collected(body: Node) -> void:
-	print("Common Item Collected.")
+	if body.has_method("add_score"):
+		body.add_score(points)
