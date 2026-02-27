f3ca08d539dfdf4d787d90fd62ff5dd4e2c8aff3
Daniel Pelley <daniel.pelley@outlook.com>
2026-02-26 18:58:47 -0600

    PROJ-21: Common Item tweaked.
    I got rid of an old merge and tweaked how the common item looked.

--- crumbling-coffers/Scripts/common_item.gd
+++ crumbling-coffers/Scripts/common_item.gd
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 extends "res://Scripts/pickup_base.gd"
 
 func _ready() -> void:
@@ -8,16 +7,3 @@ func _ready() -> void:
 func on_collected(body: Node) -> void:
 	if body.has_method("add_score"):
 		body.add_score(points)
-=======
-extends Area2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("This is a basic item.")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
->>>>>>> origin/PROJ-21-design-ability-item-system-spec
