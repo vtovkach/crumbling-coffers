c478e9974ee41085951c0aae3e596a77c1aa972b
V <vadim003600@gmail.com>
2026-02-27 23:01:07 -0600

    PROJ-32 Created entry point into the game process.
    Retrieve arguments from the parent process and invoke runGame()

---
+++ server/game/src/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+#include "game.h"
+
+
+/**
+ * @brief Entry point for the child game process.
+ *
+ * @param argc Argument count.
+ * @param argv Argument vector.
+ *
+ * Expected arguments:
+ *   argv[1] - Parent process PID (pid_t)
+ *   argv[2] - UDP game port (uint16_t)
+ *   argv[3] - pipefd[1]
+ *
+ * @return 0 on success, non-zero on error.
+ */
+int main(int argc, char *argv[])
+{
+    pid_t p_pid = (pid_t) atoi(argv[1]); 
+    uint16_t game_port = (uint16_t) atoi(argv[2]);
+    int pipe_fd = atoi(argv[3]);
+    
+    int status = runGame();
+
+    return status;
+}
