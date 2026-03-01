2d7ae0a11d168fe2b6402cb70de2972b264fe45f
V <vadim003600@gmail.com>
2026-02-28 22:17:27 -0600

    PROJ-75 Revised server-config.h

--- server/include/server-config.h
+++ server/include/server-config.h
@@ -1,11 +1,14 @@
 #ifndef _CONFIG_H
 #define _CONFIG_H
 
-#define ORCH_LOG_PATH "log/orchestrator"
-#define SERVER_TCP_PORT "10000"
-#define MAX_TCP_QUEUE 128
-#define MAX_EPOLL_EVENTS 512
-#define HASH_TABLE_SIZE 4096
-#define TCP_SEGMENT_SIZE 20
+#define ORCH_LOG_PATH           "log/orchestrator"
+#define SERVER_TCP_PORT         "10000"
+#define MAX_TCP_QUEUE           128
+#define ORCH_MAX_EPOLL_EVENTS   512
+#define HASH_TABLE_SIZE         4096
+#define TCP_SEGMENT_SIZE        20
+
+// UDP GAME PROCESS 
+#define GM_MAX_EPOLL_EVENTS     100 // will change this constant later 
 
 #endif
