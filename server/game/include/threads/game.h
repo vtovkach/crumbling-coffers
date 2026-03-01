0590781f3b42de47d2fd68cb46c85d946650c7d3
V <vadim003600@gmail.com>
2026-02-28 22:30:40 -0600

    PROJ-75 Defined network thread args structure.
    For now it only has udp_port field.
    
    Later I will add more fields.

--- server/game/include/threads/game.h
+++ server/game/include/threads/game.h
@@ -2,6 +2,15 @@
 #define __GAME_H
 #include <stdint.h>
 
+struct NetThreadArgs
+{
+    uint16_t udp_port; 
+
+    // More fields will be defined later 
+    // ...
+    
+};
+
 int runGame(uint16_t game_port);
 
 #endif
