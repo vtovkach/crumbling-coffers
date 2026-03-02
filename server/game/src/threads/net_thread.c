60962a8f1b7a33bd9ac158b442ab9327bd600ae0
V <vadim003600@gmail.com>
2026-03-01 23:56:39 -0600

    PROJ-74 Added error handling to the network thread.

--- server/game/src/threads/net_thread.c
+++ server/game/src/threads/net_thread.c
@@ -13,35 +13,36 @@
 #include "server-config.h"
 #include "threads/game.h"
 
-extern _Atomic bool stop_net;
+extern _Atomic bool stop_net; // Parent signals when to stop netThread  
 
 _Atomic bool net_dead = false;
 
 void *netThread(void *arg)
 {   
     struct NetThreadArgs args = *(struct NetThreadArgs *)arg; 
+    int listen_fd = -1; 
+    int epoll_fd = -1;
     
     // Detach from the parent 
     pthread_detach(pthread_self());
 
-    int listen_fd = make_udp_server_socket(args.udp_port);
+    listen_fd = make_udp_server_socket(args.udp_port);
     if(listen_fd == -1)
     {
-        // Something wrong TODO 
-        // 
-        printf("[netThread] make_udp_server failed.\n");
-        free(arg);
-        return NULL;
+        // Something is wrong
+        printf("[netThread] make_udp_server_socket failed.\n");
+        goto exit;
     }
 
-    int epoll_fd = epoll_create1(0);
+    epoll_fd = epoll_create1(0);
     if(epoll_fd == 0)
     {
-        // Error 
-        // Will handle later 
+        // Epoll initialization failed  
+        perror("[netThread] epoll_create");
+        goto exit; 
     }
 
-    struct epoll_event eventsQueue[ORCH_MAX_EPOLL_EVENTS];
+    struct epoll_event eventsQueue[GM_MAX_EPOLL_EVENTS];
 
     struct epoll_event ev; 
     ev.events = EPOLLIN;
@@ -49,22 +50,21 @@ void *netThread(void *arg)
 
     if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_fd, &ev) < 0)
     {
-        // TODO 
-
+        // Failed registering listen_fd with epoll 
+        perror("[netThread] epoll_ctl (listen_fd)");
+        goto exit;
     }
 
     int tick = 0;
-
     for(;;)
     {
-
         if(stop_net)
         {
-            break; 
+            printf("[netThread] parent requests thread termination.\n");
+            goto exit;  
         }
 
-        int events_ready = epoll_wait(epoll_fd, eventsQueue, ORCH_MAX_EPOLL_EVENTS, 2000);
-        
+        int events_ready = epoll_wait(epoll_fd, eventsQueue, GM_MAX_EPOLL_EVENTS, 2000);
         if(events_ready < 0)
         {
             // Error 
@@ -76,13 +76,16 @@ void *netThread(void *arg)
             struct epoll_event cur_event = eventsQueue[i];
 
             udp_read(listen_fd);
-            
         }
         
         sleep(2);
         printf("[net thread] Tick: %d\n", tick++);
     }
 
+exit: 
+    if(epoll_fd != -1)  { close(epoll_fd);  }
+    if(listen_fd != -1) { close(listen_fd); }
     free(arg);
+    net_dead = true;
     return NULL;
 }
