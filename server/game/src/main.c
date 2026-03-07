39f4ffc6fff728d9af3dacc6d479f235d3ffb864
V <vadim003600@gmail.com>
2026-03-06 18:21:49 -0600

    PROJ-76 Changed game process input arguments. Installed signal for parent process.

--- server/game/src/main.c
+++ server/game/src/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <sys/types.h>
 
 #include "threads/game.h"
-
+#include "signals.h"
 
 /**
  * @brief Entry point for the child game process.
@@ -13,9 +14,7 @@
  * @param argv Argument vector.
  *
  * Expected arguments:
- *   argv[1] - Parent process PID (pid_t)
- *   argv[2] - UDP game port (uint16_t)
- *   argv[3] - pipefd[1]
+ *   argv[1] - UDP game port (uint16_t)
  *
  * @return 0 on success, non-zero on error.
  */
@@ -23,9 +22,9 @@ int main(int argc, char *argv[])
 {
     (void) argc; // Silence compiler warning 
 
-    pid_t p_pid = (pid_t) atoi(argv[1]); 
+    signals_install(SIGUSR1); 
+
     uint16_t game_port = (uint16_t) atoi(argv[2]);
-    int pipe_fd = atoi(argv[3]);
     
     int status = runGame(game_port);
 
@@ -33,4 +32,4 @@ int main(int argc, char *argv[])
 }
 
 // Use waitpid(... WNOHANG) from orchestrator to track when game processes terminated 
-// Use signal to notify child to finish 
+// Use signal to notify child to terminate process 
