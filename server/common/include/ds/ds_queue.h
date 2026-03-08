187d8aa8eb007a0df297f443e4c2d73445ffa8cc
V <vadim003600@gmail.com>
2026-03-07 22:35:24 -0600

    PROJ-91 Imported header file for queue data structure.

---
+++ server/common/include/ds/ds_queue.h
@@ -0,0 +1,43 @@
+/*
+ * ds_lib-c - Data Structures Library
+ * Designed and implemented by Vadym Tovkach
+ * Repository: https://github.com/vtovkach/ds-lib-c/tree/main
+ *
+ * Copyright (c) 2025 Vadym Tovkach
+ * Licensed under the MIT License.
+ *
+ */
+
+#ifndef _QUEUE_
+#define _QUEUE_
+
+#include <stddef.h>
+
+typedef struct Queue Queue; 
+
+
+Queue *q_init(size_t initial_capacity, size_t data_size);
+
+void q_destroy(Queue *q_ptr);
+
+int q__enqueue(Queue *q_ptr, void *data);
+
+int q_dequeue(Queue *q_ptr, void *dest);
+
+int q_peek(Queue *q_ptr, void *dest);
+
+size_t q_size(Queue *q_ptr);
+
+size_t q_capacity(Queue *q_ptr);
+
+int q_empty(Queue *q_ptr);
+
+int q_full(Queue *q_ptr);
+
+int q_clear(Queue *q_ptr);
+
+#define q_enqueue(q_ptr, data_type, data) \
+     q__enqueue(q_ptr, &(data_type){(data)})
+
+
+#endif
