ae8a3a0d2db22fc6928e39c9735626bc3c96fc00
V <vadim003600@gmail.com>
2026-03-18 04:36:36 -0500

    PROJ-96 Added _ready and _process functions.

--- crumbling-coffers/Scripts/MainMenu/lobby.gd
+++ crumbling-coffers/Scripts/MainMenu/lobby.gd
@@ -1,5 +1,12 @@
-extends Node
+extends Control
 
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Menu/main_menu.tscn")
