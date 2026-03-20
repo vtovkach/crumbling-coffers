1a1787a7bc6b76e8cc53d9dd0ccf7eec714f60b1
Nicholas Krustchinsky <efa26@txstate.edu>
2026-03-20 09:15:39 -0500

    PROJ-80: initial state machine implementation

--- crumbling-coffers/Scripts/State/StateMachine.gd
+++ crumbling-coffers/Scripts/State/StateMachine.gd
@@ -1,1 +1,17 @@
 extends Node
+
+var active_state : State
+var states : Dictionary = {}
+
+func _ready() -> void:
+	for child in get_children():
+		if child is State:
+			states[child.name] = child
+
+func _process(delta: float) -> void:
+	if active_state:
+		active_state.update(delta)
+
+func _physics_process(delta: float) -> void:
+	if active_state:
+		active_state.update_physics(delta)
