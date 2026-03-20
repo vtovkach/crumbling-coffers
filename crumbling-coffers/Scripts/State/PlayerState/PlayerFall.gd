5d750dccf65a15aab38fe887ff7db0ef71daf757
Nicholas Krustchinsky <efa26@txstate.edu>
2026-03-20 16:07:30 -0500

    PROJ-80: remove unused functions from states (for now)

--- crumbling-coffers/Scripts/State/PlayerState/PlayerFall.gd
+++ crumbling-coffers/Scripts/State/PlayerState/PlayerFall.gd
@@ -3,15 +3,6 @@ class_name PlayerFall
 
 @export var player_character: Player
 
-func enter() -> void:
-	pass#placeholder
-	
-func exit() -> void:
-	pass#placeholder
-	
-func update(delta: float) -> void:
-	pass#placeholder
-	
 func physics_update(delta: float) -> void:
 	pass#placeholder
 	
