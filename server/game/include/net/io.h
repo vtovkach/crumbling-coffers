07044c4d7f3697ef85f741284033db3f065dc850
V <vadim003600@gmail.com>
2026-03-23 18:31:24 -0500

    PROJ-77 Redesigned I/O API

--- server/game/include/net/io.h
+++ server/game/include/net/io.h
@@ -1,8 +1,12 @@
 #ifndef _IO_
 #define _IO_
 
-ssize_t udp_read(int target_fd);
+#include <stddef.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-ssize_t udp_write();
+ssize_t udp_read(int target_fd, struct sockaddr_in *addr, void *dest, size_t dest_size);
+
+ssize_t udp_write(int target_fd, struct sockaddr_in *addr, void *buf, size_t buf_size);
 
 #endif
