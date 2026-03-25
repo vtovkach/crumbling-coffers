d043f9a21ed08afc9d6dbc6fe4db7440b742f76d
Andrea Gomez <twe27@txstate.edu>
2026-03-25 03:03:32 -0500

    PROJ-105: Implementing visual selected item update
    When pressing right click over a selected item, it will decrement the amount by 1 for now.
    
    NOTE: There is an error when trying to clear the total slot. Will need to debug in future commits.

--- crumbling-coffers/Inventory/Sub_Inventory/hotbar.gd
+++ crumbling-coffers/Inventory/Sub_Inventory/hotbar.gd
@@ -3,9 +3,12 @@ class_name Hotbar
 
 # Signal will be used to be sent out and then the changes to update the UI will be made and visible.
 signal update
+# Signal used to send out when an item is used.
+signal item_used(index)
 
 # Same insertion functionality as inventory.gd. Using different name conventions.
 @export var hotbar_slots: Array[HotbarSlot]
+var EMPTY_SLOT = null
 
 func hotbar_insert(item: HotbarItem):
 	var itemSlots = hotbar_slots.filter(func(slot): return slot.hotbar_item == item) 
@@ -18,3 +21,13 @@ func hotbar_insert(item: HotbarItem):
 			emptySlots[0].amount = 1
 	
 	update.emit()
+
+func _on_item_used(index):
+	if hotbar_slots[index].amount > 1:
+		hotbar_slots[index].amount -= 1
+		update.emit()
+	elif hotbar_slots[index].amount == 1:
+		# DO NOTHING FOR NOW - NEED TO DEBUG ERROR SO BASE OBJECT IS NOT NULL.
+		#hotbar_slots[index] = EMPTY_SLOT
+		update.emit()
+		
--- crumbling-coffers/Inventory/Sub_Inventory/hotbar_ui.gd
+++ crumbling-coffers/Inventory/Sub_Inventory/hotbar_ui.gd
@@ -14,6 +14,8 @@ func _ready():
 	# call the updates to have scene be up to date when first ran.
 	update_hotbar_slots()
 	update_active_item_visual()
+	#connect the item_used signal
+	hotbar.item_used.connect(hotbar._on_item_used)
 	
 
 # the update function for the hotbar will be the same functionality as the inventory's.
@@ -21,7 +23,7 @@ func update_hotbar_slots():
 	for i in range(min(hotbar.hotbar_slots.size(), slots.size())):
 		slots[i].update(hotbar.hotbar_slots[i])
 		# calling to ensure that the texture is swapped to the highlighted slot.
-		update_active_item_visual()
+	update_active_item_visual()
 
 func update_active_item_visual():
 	for i in range(slots.size()):
@@ -42,3 +44,24 @@ func active_item_scroll_down():
 		active_item_slot -= 1
 	
 	active_item_updated.emit()
+
+# Will check if the button (right mouse click) is pressed to use the highlighted item.
+# Will only remove the selected item from the array for now.
+func _input(event):
+	# Adding in the ability to use an item -- will only currently remove it from hotbar.
+	if event.is_action_pressed("item_used"):
+		use_active_item()
+
+
+func use_active_item():
+	var selected_item = hotbar.hotbar_slots[active_item_slot]
+	
+	if selected_item == null:
+		return # return to ensure no game crash -- does nothing.
+	
+	# This function will be implemented to have the item affect the player/other players.
+	# Not defined for now. Need to define in the future.
+	# apply_item_effect(selected_item)
+	
+	# Send out signal that an item was used.
+	hotbar.item_used.emit(active_item_slot)
