065a0ae46195f4f1e5ebcf9d3f950da00ac6c86c
Daniel Pelley <daniel.pelley@outlook.com>
2026-03-25 14:09:55 -0500

    PROJ-111: adding freeze component to test dummy.

--- crumbling-coffers/Scenes/TestDummy/test_dummy.gd
+++ crumbling-coffers/Scenes/TestDummy/test_dummy.gd
@@ -2,12 +2,28 @@ extends CharacterBody2D
 
 const SPEED = 600.0
 const GRAVITY = 900.0
-
 var direction = 1
+
+var is_frozen: bool = false
+var freeze_time_left: float = 0.0
 
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCast2Left
 
+
+
+func apply_freeze(duration: float) -> void:
+	is_frozen = true
+	freeze_time_left = duration
+
+func clear_freeze() -> void:
+	is_frozen = false
+	freeze_time_left = 0.0
+
+func _ready() -> void:
+		add_to_group("freezable")
+		
+	
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
