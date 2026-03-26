f7fdab0a84d86e1461c57c497754e6d2dd676deb
Nicholas Krustchinsky <efa26@txstate.edu>
2026-03-26 17:35:20 -0500

    PROJ-27: add indicator add-and-remove to ItemIndicatorManager
    Not all implementation included

--- crumbling-coffers/Scripts/VisualIndicators/ItemIndicatorManager.gd
+++ crumbling-coffers/Scripts/VisualIndicators/ItemIndicatorManager.gd
@@ -16,7 +16,24 @@ func _process(delta: float) -> void:
 	# Yes... every frame get all the pickups. I am aware that this MIGHT NOT be a great idea. 
 	# This system integrates a lot of moving and misplaced parts so I'm willing to make a convenient inefficiency
 	var items = get_tree().get_nodes_in_group("pickups")
+	_spawn_indicators(items)
+	_destroy_indicators(items)
+
+func _spawn_indicators(items: Array) -> void:
+	pass	# NOT IMPLEMENTED
 	
+func _destroy_indicators(items: Array) -> void:
+	var valid_items: Dictionary[PickupBase, bool] = {}
+
+	for item in items:
+		valid_items[item] = true
+
+	for item in indicators.keys():
+		if not valid_items.has(item):
+			_remove_indicator(item)
+
+func _remove_indicator(item: PickupBase) -> void:
+	pass	# NOT IMPLEMENTED
 
 func setPlayer(p: Player) -> void:
 	player = p
