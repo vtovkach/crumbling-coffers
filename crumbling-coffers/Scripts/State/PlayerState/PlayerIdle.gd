3890f8ee6aa90f06b714d20a47ae799f7353f180
Nicholas Krustchinsky <efa26@txstate.edu>
2026-03-27 02:20:23 -0500

    PROJ-116: Re-order jump vs falling states
    Jump priority

--- crumbling-coffers/Scripts/State/PlayerState/PlayerIdle.gd
+++ crumbling-coffers/Scripts/State/PlayerState/PlayerIdle.gd
@@ -5,12 +5,13 @@ class_name PlayerIdle
 
 func physics_update(delta: float) -> void:
 	player.move(0, delta)
-	if not player.is_on_floor():
-		transitioned.emit(self, "PlayerFall")
-		return
-	elif player.jump_pressed:
+
+	if player.jump_pressed:
 		transitioned.emit(self, "PlayerJump")
 		return
-	if player.direction != 0:
+	elif not player.is_on_floor():
+		transitioned.emit(self, "PlayerFall")
+		return
+	elif player.direction != 0:
 		transitioned.emit(self, "PlayerRun")
 		return
--- crumbling-coffers/Scripts/State/PlayerState/PlayerRun.gd
+++ crumbling-coffers/Scripts/State/PlayerState/PlayerRun.gd
@@ -5,9 +5,10 @@ class_name PlayerRun
 
 func physics_update(delta: float) -> void:
 	player.move(player.direction, delta)
-	if not player.is_on_floor():
-		transitioned.emit(self, "PlayerFall")
-		return
-	elif player.jump_pressed:
+
+	if player.jump_pressed:
 		transitioned.emit(self, "PlayerJump")
+		return
+	elif not player.is_on_floor():
+		transitioned.emit(self, "PlayerFall")
 		return
