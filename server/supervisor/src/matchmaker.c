e464dca17bc04f9c3fab3391acaf0652342b37fb
V <vadim003600@gmail.com>
2026-04-04 00:23:20 -0500

    PROJ-135: Add shutdown loop stub to matchmaker

--- server/supervisor/src/matchmaker.c
+++ server/supervisor/src/matchmaker.c
@@ -1,12 +1,18 @@
-#include <stdio.h>
-#include <stddef.h>
 #include <stdatomic.h>
+#include <unistd.h>
+#include <stdio.h>
 
 extern atomic_bool shutdown; 
 
 void *matchmaker_run_t(void *args)
 {   
     (void)args; // Silence compiler warning 
+
+    while(!atomic_load(&shutdown))
+    {
+        printf("matchmaker\n");        
+        sleep(1);
+    }
 
     return NULL;
 }
