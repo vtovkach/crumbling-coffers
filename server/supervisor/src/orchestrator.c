15351f54d5c514a167bbe364bc0b9c29a503bc0d
V <vadim003600@gmail.com>
2026-04-04 00:23:21 -0500

    PROJ-135: Add shutdown loop stub to orchestrator

--- server/supervisor/src/orchestrator.c
+++ server/supervisor/src/orchestrator.c
@@ -1,11 +1,19 @@
 #include <stdatomic.h>
+#include <unistd.h>
 #include <stdio.h>
-#include <stddef.h>
+
 
 extern atomic_bool shutdown; 
 
 void *orch_run_t(void *args)
 {
     (void)args; // Silence compiler warning 
+
+    while(!atomic_load(&shutdown))
+    {
+        printf("orch\n");       
+        sleep(1);
+    }
+
     return NULL;
 }
