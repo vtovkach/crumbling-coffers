ffbf6c3551910110476a7fcb474aa92b7b783348
V <vadim003600@gmail.com>
2026-04-05 18:10:37 -0500

    PROJ-135 Updated tcp packet header file

---
+++ server/supervisor/include/tcp_packets.h
@@ -0,0 +1,41 @@
+#ifndef _SV_PACKET_H_
+#define _SV_PACKET_H_
+
+#include "server-config.h"
+
+#include <stdint.h>
+
+#define SV_EVENT_MATCH_REQUEST  0
+#define SV_EVENT_MATCH_CANCEL   1
+
+/*
+ * Incoming TCP packet from client.
+ * 4 bytes: request type code.
+ *   0 = match request
+ *   1 = match cancel
+ */
+struct __attribute__((packed)) IncomingPacket
+{
+    uint32_t event_type;
+};
+
+/*
+ * Outgoing TCP packet from client.
+ * 4 bytes: Packet Type code 
+ *   2 = match found 
+ *   3 = match not found 
+ * 
+ * GAME_ID_SIZE(16 bytes) bytes: Game Id as raw bytes 
+ * 
+ * 4 bytes: Game Server IP
+ * 2 bytes: Game Port 
+ */
+struct __attribute__((packed)) OutgoingPacket
+{
+    uint32_t event_type;
+    uint8_t game_id[GAME_ID_SIZE];
+    uint32_t server_ip;
+    uint16_t server_port; 
+};
+
+#endif
