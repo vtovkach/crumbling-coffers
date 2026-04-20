6a5c8424a00934122f487e04f24c21628076caee
V <vadim003600@gmail.com>
2026-04-20 14:42:40 -0500

    PROJ-160 Update test file to reflect timer changes.

--- crumbling-coffers/Scripts/Game/test_game.gd
+++ crumbling-coffers/Scripts/Game/test_game.gd
@@ -5,8 +5,9 @@ const LOCAL_ID: String = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 func _ready() -> void:
 	var response := PacketizationManager.UDP_Response.new()
 	response.start_tick = 0
-	response.stop_tick  = 999999
+	response.stop_tick  = 5000
 	response.player_init_positions[LOCAL_ID] = PacketizationManager.PlayerInitPos.new(-425, -200)
 
 	init(LOCAL_ID, "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB", 34567, response)
+	game_status = GameStatus.RUNNING
 	local_player.set_physics_process(true)
