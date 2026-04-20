1218a40f6570342502e88a84f89b08418a35c4c8
V <vadim003600@gmail.com>
2026-04-20 14:48:46 -0500

    PROJ-160 Reduced time for test game

--- crumbling-coffers/Scripts/Game/test_game.gd
+++ crumbling-coffers/Scripts/Game/test_game.gd
@@ -5,7 +5,7 @@ const LOCAL_ID: String = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 func _ready() -> void:
 	var response := PacketizationManager.UDP_Response.new()
 	response.start_tick = 0
-	response.stop_tick  = 5000
+	response.stop_tick  = 500
 	response.player_init_positions[LOCAL_ID] = PacketizationManager.PlayerInitPos.new(-425, -200)
 
 	init(LOCAL_ID, "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB", 34567, response)
