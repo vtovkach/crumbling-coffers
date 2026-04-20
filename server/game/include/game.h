7a452180c5dbdc18fb20a4f2eef8c5b4c4cf1ea3
V <vadim003600@gmail.com>
2026-04-20 00:32:26 -0500

    PROJ-151: Updated Game struct with float positions, Player** array, and update_game signature

--- server/game/include/game.h
+++ server/game/include/game.h
@@ -12,6 +12,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#define INIT_POSITIONS_COUNT 7
+
 enum GameStatus
 {
     NOT_READY,
@@ -19,19 +21,25 @@ enum GameStatus
     STARTED
 };
 
+struct InitPos
+{
+    float pos_x;
+    float pos_y;
+};
+
 struct Game
 {
     uint8_t game_id[GAME_ID_SIZE];
+    uint32_t game_tick;
+    enum GameStatus status;
     uint16_t map_id;
+    struct InitPos initial_positions[INIT_POSITIONS_COUNT];
 
     size_t players_num;
-    struct Player *players;
+    struct Player **players;
 
     size_t items_num;
     HashTable *items;
-
-    uint32_t game_tick;
-    enum GameStatus status; 
 };
 
 struct Game *create_game(uint8_t *game_id, uint16_t map_id, size_t players_num, FILE *log_file);
@@ -40,7 +48,7 @@ void destroy_game(struct Game *game, FILE *log_file);
 
 void add_player(struct Game *game, struct Player *player);
 
-void update_game(struct Game *game);
+void update_game(struct Game *game, const struct Packet *pkt);
 
 void form_auth_packet(struct Game *game, uint32_t start_tick, uint32_t stop_tick, struct AuthPacket *dst);
 
